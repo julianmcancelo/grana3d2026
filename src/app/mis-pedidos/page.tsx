89dba7208d56906eec333953b445bc40e66e65db
Julian Cancelo <julianmcancelo@gmail.com>
2026-02-06 23:20:57 -0300

    fix: forzar modo dinamico en mis pedidos para evitar error de build

--- src/app/mis-pedidos/page.tsx
+++ src/app/mis-pedidos/page.tsx
@@ -2,6 +2,9 @@ import { getGlobalConfig } from '@/lib/config'
 import MantenimientoEpicardo from '@/components/MantenimientoEpicardo'
 import MisPedidosClient from './MisPedidosClient'
 
+// Forzar renderizado dinámico para evitar conexión a DB en build time
+export const dynamic = 'force-dynamic'
+
 export default async function MisPedidosPage() {
     const config = await getGlobalConfig()
 
